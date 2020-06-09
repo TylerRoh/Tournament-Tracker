166e216dd46c94393a39897531f3899fe9725f71
Tyler Rohweder <trohweder@pfcequip.com>
2020-06-09 14:04:51 -0500

    Added notes to the text methods

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -16,7 +16,7 @@ namespace TrackerLibrary.TextHelpers
         /// <summary>
         /// Saves a new prize to the txt doc
         /// </summary>
-        /// <param name="model"></param>
+        /// <param name="model">prize model</param>
         /// <returns>The prize information, inluding unique identifier</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
@@ -40,6 +40,11 @@ namespace TrackerLibrary.TextHelpers
             return model;
         }
 
+        /// <summary>
+        /// Creates Person models based off of data stored in the People text file.
+        /// </summary>
+        /// <param name="model">person model</param>
+        /// <returns></returns>
         public PersonModel CreatePerson(PersonModel model)
         {
             //Loads people.csv and converts to list of PersonModel
@@ -62,11 +67,20 @@ namespace TrackerLibrary.TextHelpers
             return model;
         }
 
+        /// <summary>
+        /// returns all lines in the PeopleFile converted to person models
+        /// </summary>
+        /// <returns>A list of PersonModel</returns>
         public List<PersonModel> GetPerson_All()
         {
             return GlobalConfig.PeopleFile.FullFilePath().LoadFile().ConvertToPersonModels();
         }
 
+        /// <summary>
+        /// Creates team model, saves it to Teams text file
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         public TeamModel CreateTeam(TeamModel model)
         {
             List<TeamModel> teams = GlobalConfig.TeamsFile.FullFilePath().LoadFile().ConvertToTeamModels();
@@ -88,11 +102,19 @@ namespace TrackerLibrary.TextHelpers
 
         }
 
+        /// <summary>
+        /// Reads all lines of Teams text file and returns as TeamModels
+        /// </summary>
+        /// <returns>List of Team Models</returns>
         public List<TeamModel> GetTeam_All()
         {
             return GlobalConfig.TeamsFile.FullFilePath().LoadFile().ConvertToTeamModels();
         }
 
+        /// <summary>
+        /// Saves tournament model, matchups, and entries to text file.
+        /// </summary>
+        /// <param name="model"></param>
         public void CreateTournament(TournamentModel model)
         {
             List<TournamentModel> tournaments = GlobalConfig.TournamentsFile
@@ -117,6 +139,10 @@ namespace TrackerLibrary.TextHelpers
 
         }
 
+        /// <summary>
+        /// Reads all lines in the tournament, matchup and entries text files and returns them as a list of tournament models
+        /// </summary>
+        /// <returns>List of all tournament models</returns>
         public List<TournamentModel> GetTournament_All()
         {
             return GlobalConfig.TournamentsFile.FullFilePath().LoadFile().ConvertStringsToTournamentModels();
